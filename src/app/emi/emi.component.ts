4075be45e3564dbc1048edf9dc9e5058f64253c9
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-03-14 00:09:11 +0530

    Emi HTML CSS

---
+++ src/app/emi/emi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emi',
+  standalone: true,
+  imports: [],
+  templateUrl: './emi.component.html',
+  styleUrl: './emi.component.css'
+})
+export class EmiComponent {
+
+}

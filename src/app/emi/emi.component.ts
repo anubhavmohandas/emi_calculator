c4575d6e22cd440747d3a872bb249a71afcbb137
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-03-26 23:32:40 +0530

    Working

--- src/app/emi/emi.component.ts
+++ src/app/emi/emi.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-emi',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './emi.component.html',
-  styleUrl: './emi.component.css'
+  styleUrls: ['./emi.component.css']
 })
 export class EmiComponent {
-
+  principal: number = 0;
+  roi: number = 0;
+  t: number = 0;
+  emi: number = 0;
+  interest: number = 0;
+  gst: number = 18; 
+  calculateEMI(principal: number, roi: number, t: number, gst: number) {
+    this.principal = principal;
+    this.roi = roi;
+    this.t = t;
+    this.gst = gst;
+    
+    // EMI Calculation
+    const totalInterest = (this.principal * this.roi * this.t) / 100;
+    this.interest = totalInterest;
+    const totalAmount = this.principal + totalInterest;
+    const totalGST = (totalAmount * this.gst) / 100;
+    this.emi = (totalAmount + totalGST) / (this.t * 12);
+  }
 }
